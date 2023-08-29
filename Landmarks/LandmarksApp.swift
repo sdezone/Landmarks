5572bc8438246cdac76b8565ee7bb8c8b595f22c
sdezone <mh.nusretkaya@hotmail.com>
2023-08-29 16:29:07 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Nusret Kaya on 29.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
